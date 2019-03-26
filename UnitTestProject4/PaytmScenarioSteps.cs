1630aae05277b6ac22d93f46bf299d89703a1740
Anirudh Desai <EE209973@sasken.com>
2019-03-26 12:42:25 +0530

    POM

---
+++ UnitTestProject4/PaytmScenarioSteps.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+
+namespace UnitTestProject4
+{
+    [Binding]
+    public class PaytmScenarioSteps
+
+    {
+        bool ElementNotFound;
+
+        PaytmHomePagePOM paytmPom = new PaytmHomePagePOM();
+
+        [Given(@"I have entered Paytm website")]
+        public void GivenIHaveEnteredPaytmWebsite()
+        {
+            paytmPom.NavigateToHomePageOfPaytm();
+        }
+
+
+        [Given(@"I click on Mobile option")]
+        public void GivenIClickOnMobileOption()
+        {
+            paytmPom.ClickOnMobileButton();
+        }
+
+        [Given(@"also I have entered Mobile number and Amount")]
+        public void GivenAlsoIHaveEnteredMobileNumberAndAmount()
+        {
+            paytmPom.EnterMobileNumberAndAmount();
+        }
+
+        [When(@"I click on Proceed Recharge Button")]
+        public void WhenIClickOnProceedRechargeButton()
+        {
+            paytmPom.ClickOnProceedRechargeButton();
+        }
+
+        [When(@"fields like Mobile,Electricity,DTH,Metro are pesent")]
+        public void WhenFieldsLikeMobileElectricityDTHMetroArePesent()
+        {
+            try
+            {
+                paytmPom.driver.FindElement(By.XPath(XpathClass.mobile));//Mobile 
+                paytmPom.driver.FindElement(By.XPath(XpathClass.electricity));//electricity
+                paytmPom.driver.FindElement(By.XPath(XpathClass.dth));//DTH
+                paytmPom.driver.FindElement(By.XPath(XpathClass.metro));//Metro
+            }
+            catch (Exception Exp)
+            {
+                ElementNotFound = true;
+            }
+        }
+
+        [Then(@"check all the corresponding fields are present or not")]
+        public void ThenCheckAllTheCorrespondingFieldsArePresentOrNot()
+        {
+            Assert.IsFalse(ElementNotFound);
+        }
+
+
+        [Then(@"Page should navigate to Url https://paytm\.com/recharge")]
+        public void ThenPageShouldNavigateToUrlHttpsPaytm_ComRecharge()
+        {
+            Assert.AreEqual(paytmPom.GetPaytmRechargeURl(), paytmPom.GetCurrentUrl());
+        }
+
+        [Then(@"Page should Navigate to Proceed to Pay and Url should be https://paytm\.com/coupons")]
+        public void ThenPageShouldNavigateToProceedToPayAndUrlShouldBeHttpsPaytm_ComCoupons()
+        {
+            Assert.AreEqual(paytmPom.GetPaytmCouponsURl(), paytmPom.GetCurrentUrl());
+
+           
+           // Assert.IsNotNull(paytmPom.GetXpathOfCoupnPage());
+        }
+    }
+}
+
---
+++ UnitTestProject4/SpecFlowFeature1.feature
@@ -0,0 +1,23 @@
+﻿Feature: Paytm Scenario
+	
+
+@mytag
+Scenario: Open Paytm website & Validates services
+	Given I have entered Paytm website
+	When fields like Mobile,Electricity,DTH,Metro are pesent
+	Then check all the corresponding fields are present or not
+
+@mytag
+Scenario: Validate Mobile Recharge Button
+	Given I have entered Paytm website
+	And I click on Mobile option
+	Then Page should navigate to Url https://paytm.com/recharge
+
+
+@mytag
+Scenario: Perform Paytm Recharge
+	Given I have entered Paytm website
+	And I click on Mobile option
+	And also I have entered Mobile number and Amount
+	When I click on Proceed Recharge Button
+	Then Page should Navigate to Proceed to Pay and Url should be https://paytm.com/coupons
